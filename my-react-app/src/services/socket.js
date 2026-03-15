a263791303ca31dfb7f4f12e59d26c1d04aa1a93
AayushPratapBansal <aayushpratapbansal@gmail.com>
2026-03-15 12:53:33 +0530

    fix socket url fallback

--- my-react-app/src/services/socket.js
+++ my-react-app/src/services/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+// const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL || "https://chat-app-project-4.onrender.com";
 
 let socket;
 
